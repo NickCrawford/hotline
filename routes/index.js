8cda5175f1b6f57baa10f959d77f25da0faaf724
NickCrawford <hi@nickcrawford.design>
2021-06-22 18:58:10 -0400

    Adds jokes

--- routes/index.js
+++ routes/index.js
@@ -51,22 +51,117 @@ router.post("/voice-response", function (req, res, next) {
 
   // Creates an array of jokes and randomly chooses one to deliver
   const jokes = [
-    "A plateau is the highest form of flattery.",
-    "Don't trust atoms, they make up everything.",
-    "Trying to write with a broken pencil is pointless",
-    "I'm reading a book about anti-gravity. It's impossible to put down.",
-    "Want to hear a joke about paper? Nevermind, it's tear-able.",
-    "I was going to share a vegetable joke but it's corny.",
-    "Is your refrigerator running? Better go catch it!",
+    "Adopt an ant farm",
+    "Get really into lock-picking",
+    "Download TikTok",
+    "Adopt a pet raccoon",
+    "do something involving a lizard ",
+    "Get really into day trading ",
+    "Get a planet fitness membership",
+    "Buy kettlebells",
+    "Become a barista at a cute coffee shop and quit two-weeks later",
+    "Start a SoundCloud and learn to rap",
+    "Become an e-girl",
+    "Start making kombucha and forget about it",
+    "Create a quarter-life crisis hotline instead of actually getting a job",
+    "Start an alt-twitter",
+    "Move to Ohio",
+    "Move to New York",
+    "Move to Colorado",
+    "Move to California",
+    "Get a nose piercing",
+    "Get a tattoo",
+    "Dye your hair",
+    "Start listening to way too much Hozier",
+    "Buy a tiny a house",
+    "Buy a 3D printer",
+    "Start making memes for Instagram",
+    "Start a creator house",
+    "Get married too soon",
+    "Start a podcast about dating",
+    "Start a podcast about crypto",
+    "Get really into doge coin",
+    "Rob a bank",
+    "Start going to therapy",
+    "Become low-key alcoholic",
+    "Become high-key alcoholic",
+    "Quit your salaried job to become a bartender",
+    "Start brewing IPAs in your kitchen",
+    "Get really into 3D printing ",
+    "Start knitting scarves",
+    "Take a trip to Miami",
+    "Start an agency",
+    "Tell all your friends you’re going to become a pilot",
+    "Tyedye EVERYTHING in your closet",
+    "Start a depop even though you own no clothes",
+    "Start a podcast",
+    "Start doing pottery",
+    "Start a band",
+    "Start a skater girl gang",
   ];
 
-  const joke = jokes[Math.floor(Math.random() * jokes.length)];
+  const indexOne = Math.floor(Math.random() * jokes.length);
+  const indexTwo = Math.floor(Math.random() * jokes.length);
+
+  if (indexOne == indexTwo) indexTwo = Math.floor(Math.random() * jokes.length);
+
+  const voice = "Polly.Amy";
 
   // creates the text-to-speech response
-  twiml.pause({ length: 1 });
-  twiml.say(joke, {
-    voice: "alice",
+  twiml.say(
+    "Hello! Thank you for calling the Quarter-Life Crisis Hotline. I hope this message finds you well!",
+    {
+      voice: voice,
+    }
+  );
+  twiml.pause({ length: 0.5 });
+
+  twiml.say(
+    "We're here to help you figure out what you're doing with your life.",
+    {
+      voice: voice,
+    }
+  );
+  twiml.pause({ length: 0.5 });
+
+  twiml.say(
+    "Alright, give me just one second and I'll come up with some suggestions for you.",
+    {
+      voice: voice,
+    }
+  );
+  twiml.pause({ length: 2 });
+
+  twiml.say("Okay, ready! The universe is telling me that you should...", {
+    voice: voice,
   });
+
+  // Say joke one
+  twiml.say(jokes[indexOne], {
+    voice: voice,
+  });
+  twiml.pause({ length: 0.1 });
+
+  // Plus "and/or"
+  twiml.say("and", {
+    voice: voice,
+  });
+  twiml.pause({ length: 0.1 });
+
+  // Say option two
+  twiml.say(jokes[indexTwo], {
+    voice: voice,
+  });
+
+  twiml.pause({ length: 1 });
+
+  twiml.say(
+    "I hope that helps! Let us know how it works out and feel free to call back anytime. Goodbye!",
+    {
+      voice: voice,
+    }
+  );
+
   res.send(twiml.toString());
 });
 
